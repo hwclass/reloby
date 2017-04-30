67ed3d0c347882f14579faa74f308f63f9ce99c7
Barış Güler <harderworking.class@gmail.com>
2017-04-30 17:48:26 +0100

    added spinner into LoaderButton, added plain Signup page

--- reloby-app-client/src/components/LoaderButton.js
+++ reloby-app-client/src/components/LoaderButton.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Button, Glyphicon } from 'react-bootstrap';
 
+import './LoaderButton.css';
+
 export default ({ isLoading, text, loadingText, disabled = false, ...props }) => (
   <Button disabled={ disabled || isLoading } {...props}>
     { isLoading && <Glyphicon glyph="refresh" className="spinning" /> }
